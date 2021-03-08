4d8cece897ab20a79af720f096f1bb9c84d79885
Finn <10346401+finncodes@users.noreply.github.com>
2021-03-08 23:24:09 +1300

    Documented the parser and the Request class

--- geminpy/parser.py
+++ geminpy/parser.py
@@ -1,11 +1,17 @@
 import re
+from typing import Union
+
 from .request import Request
 
 # matches hostname and subsequent path
 REQUEST_PATTERN = r"gemini://([^/]+)(/.*)*\r\n"
 
 
-def parse_request(data):
+def parse_request(data: bytes) -> Union[Request, None]:
+    """Parses the bytes of an incoming request from a client's socket
+    :param      data: the bytes received from the client
+    :return:    a Request object if successfully parsed, None if not
+    """
     data_string = data.decode('utf-8')
     match = re.match(REQUEST_PATTERN, data_string)
 
--- geminpy/request.py
+++ geminpy/request.py
@@ -1,5 +1,20 @@
+from typing import Union
+
+
 class Request:
-    def __init__(self, hostname, path, query_parameters=None):
+    """
+    A class used to contain the data of an incoming request from a client
+
+    :ivar hostname:         the hostname the client connected to
+    :ivar path:             the URI of the requested resource
+    :ivar query_parameters: the values extracted from the query string
+    """
+    def __init__(self, hostname: str, path: str, query_parameters: Union[None, list[str]] = None):
+        """ The constructor for the Request class
+        :param hostname:            the hostname of the server
+        :param path:                the URI of the requested resource
+        :param query_parameters:    the query parameters in a list
+        """
         if query_parameters is None:
             query_parameters = []
         self.hostname = hostname
